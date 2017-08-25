6886e59849583086b96ec6ddaf8fce3c114ea653
Unknown <897142843@qq.com>
2017-08-25 18:22:50 +0800

    修复了路由导致数据无法传输的问题

--- routes/auditUser.js
+++ routes/auditUser.js
@@ -13,7 +13,7 @@ router.post('/auditUserData', function(req, res, next) {
     
     var searchCondition = req.body.searchCondition;
 
-    var sql = "select acc,pwd,name,studentID from userAcc natural join baseInformation where acc like'%"+searchCondition+"%' or pwd like'%"+searchCondition+"%' or name like'%"+searchCondition+"%' or studentID like'%"+searchCondition+"%' ;";
+    var sql = "select acc,status,name,studentID from userAcc natural join baseInformation where acc like'%"+searchCondition+"%' or status like'%"+searchCondition+"%' or name like'%"+searchCondition+"%' or studentID like'%"+searchCondition+"%' ;";
     var sqlParams = [];
 
     connection.query(sql,sqlParams,function(err,result){
@@ -46,46 +46,46 @@ router.post('/auditUserData', function(req, res, next) {
 });
 
 //返回详细信息
-router.post('/userAudit', function(req, res, next) {
+router.post('/userDetail', function(req, res, next) {
     
     console.log(req.body);
 
-    // var sql = " select photoaddress,studentCard,nickname,name,sex,college,studentID from useracc natural join baseInformation where acc = ?";
-    // var sqlParams = [req.body.acc];
+    var sql = " select photoaddress,studentCard,nickname,name,sex,college,studentID,status from useracc natural join baseInformation where acc = ?";
+    var sqlParams = [req.body.acc];
 
-    // connection.query(sql,sqlParams,function(err,result){
+    connection.query(sql,sqlParams,function(err,result){
 
-    //     if(err){
-    //         console.log('error:',err.message);
-    //         return;
-    //     }
+        if(err){
+            console.log('error:',err.message);
+            return;
+        }
 
-    //     var json={};
-    //     //console.log(result);
+        var json={};
+        //console.log(result);
 
-    //     if(result.length == 0){
-    //         //查不到
+        if(result.length == 0){
+            //查不到
             
 
-    //     }else{
-    //         //能查询到
-    //          json['photo'] =result[0].photoaddress;
-    //          json['studentCard'] =result[0].studentCard;
-    //          json['acc'] = req.body.acc;
-    //          json['nickname'] =result[0].nickname ;
-    //          json['credit'] =result[0].credit ;
-    //          json['name'] =result[0].name ;
-    //          json['sex'] =result[0].sex ;
-    //          json['college'] =result[0].college ;
-    //          json['studentID'] =result[0].studentID ;
-    //          console.log(json);
-    //     }
+        }else{
+            //能查询到
+             json['photo'] =result[0].photoaddress;
+             json['studentCard'] =result[0].studentCard;
+             json['acc'] = req.body.acc;
+             json['nickname'] =result[0].nickname ;
+             json['status'] =result[0].status ;
+             json['name'] =result[0].name ;
+             json['sex'] =result[0].sex ;
+             json['college'] =result[0].college ;
+             json['studentID'] =result[0].studentID ;
+             console.log(json);
+        }
 
-    //     res.send(json);
-    //     res.end("");
+        res.send(json);
+        res.end("");
 
 
-    // });
+    });
 
 });
 
